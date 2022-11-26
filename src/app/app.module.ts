f28b8dd4bf1bcccb4d3d71cd565c97c535ed3a46
Agata Gryszkiewicz <gryszkiewicz.agata@gmail.com>
2022-11-26 11:15:35 +0100

    feat: Task 3, add service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeeListComponentModule } from "./ui/employee-list/employee-list.component-module";
 import {EmployeeFacesComponentModule} from "./ui/employee-faces/employee-faces.component-module";
+import {EmployeeServiceModule} from "./services/employee.service-module";
 
 @NgModule({
   declarations: [AppComponent],
@@ -13,6 +14,7 @@ import {EmployeeFacesComponentModule} from "./ui/employee-faces/employee-faces.c
     AppRoutingModule,
     HttpClientModule,
     EmployeeListComponentModule,
+    EmployeeServiceModule,
     EmployeeFacesComponentModule,
   ],
   providers: [],
